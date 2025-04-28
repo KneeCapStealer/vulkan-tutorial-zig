fdf82d5fc2b99104a137b25c9658688d5a1cf7b7
Christoffer Hald Christensen <chrishald@proton.me>
2025-04-29 01:10:04 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Vulkan tutorial done in zig";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://chaotic-nyx.cachix.org/"
+    ];
+    extra-trusted-public-keys = [
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { system, pkgs, ... }:
+        {
+          # Use chaotic overlay
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.chaotic.overlays.default ];
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+
+          devShells.default = pkgs.mkShell {
+            packages =
+              with pkgs.vulkanPackages_latest;
+              [
+                vulkan-headers
+                vulkan-loader
+                vulkan-validation-layers
+              ]
+              ++ (with pkgs; [
+                zig
+              ]);
+
+            shellHook = ''
+              mkdir -p ./vulkan
+              cp -f ${pkgs.vulkanPackages_latest.vulkan-headers}/share/vulkan/registry/vk.xml ./vulkan
+            '';
+          };
+        };
+    };
+}
