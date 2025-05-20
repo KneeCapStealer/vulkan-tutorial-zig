49ae1f0925ace7e30788030fbb1a29dd535f08d2
Christoffer Hald Christensen <chrishald@proton.me>
2025-05-21 00:45:31 +0200

    Added validation layers and debug utils extensions

--- src/App.zig
+++ src/App.zig
@@ -1,13 +1,24 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const mem = std.mem;
+
 const glfw = @import("glfw");
 const vk = @import("vulkan");
 
 const App = @This();
 
-const allocator = if (@import("builtin").mode == .Debug) (std.heap.DebugAllocator(.{}){}).allocator() else std.heap.smp_allocator;
+var debugAllocator = std.heap.DebugAllocator(.{}){};
+const allocator = if (builtin.mode == .Debug) debugAllocator.allocator() else std.heap.smp_allocator;
 
 const width = 800;
 const height = 600;
+
+// Validation layers
+const enable_val_layers = builtin.mode == .Debug;
+/// Layer names to load if validation layers are active
+const val_layers: []const [*:0]const u8 = &.{
+    "VK_LAYER_KHRONOS_validation",
+};
 
 window: *glfw.Window,
 vk_base: vk.BaseWrapper,
@@ -40,6 +51,15 @@ fn initWindow(self: *App) !void {
 }
 
 fn createInstance(self: *App) !void {
+    if (enable_val_layers and !try self.checkValLayerSupport()) {
+        return error.ValidationLayersNotAvailable;
+    }
+
+    const enabled_layer_count: u32, const enabled_layer_names: ?[*]const [*:0]const u8 = if (enable_val_layers)
+        .{ val_layers.len, val_layers.ptr }
+    else
+        .{ 0, null };
+
     const appInfo: vk.ApplicationInfo = .{
         .p_application_name = "Hello Triangle",
         .p_engine_name = "No Engine",
@@ -48,14 +68,13 @@ fn createInstance(self: *App) !void {
         .api_version = @bitCast(vk.API_VERSION_1_0),
     };
 
-    var glfw_extension_count: u32 = undefined;
-    const glfw_extension = glfw.getRequiredInstanceExtensions(&glfw_extension_count);
-
+    const extensions = try getRequiredExtensions();
     const instance_create_info: vk.InstanceCreateInfo = .{
         .p_application_info = &appInfo,
-        .enabled_layer_count = 0,
-        .enabled_extension_count = glfw_extension_count,
-        .pp_enabled_extension_names = glfw_extension,
+        .enabled_layer_count = enabled_layer_count,
+        .pp_enabled_layer_names = enabled_layer_names,
+        .enabled_extension_count = @intCast(extensions.len),
+        .pp_enabled_extension_names = extensions.ptr,
     };
     const instance = try self.vk_base.createInstance(&instance_create_info, null);
     self.instance_wrapper = .load(instance, self.vk_base.dispatch.vkGetInstanceProcAddr.?);
@@ -78,6 +97,38 @@ fn cleanup(self: *App) !void {
     self.vk_instance.destroyInstance(null);
     glfw.destroyWindow(self.window);
     glfw.terminate();
+}
+
+fn checkValLayerSupport(self: *App) !bool {
+    const available_layers = try self.vk_base.enumerateInstanceLayerPropertiesAlloc(allocator);
+
+    outer: for (val_layers) |layer_name| {
+        const layer_name_len = mem.len(layer_name);
+
+        for (available_layers) |layer| {
+            if (mem.eql(u8, mem.span(layer_name), layer.layer_name[0..layer_name_len])) {
+                continue :outer;
+            }
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+fn getRequiredExtensions() ![]const [*:0]const u8 {
+    var glfw_extension_count: u32 = undefined;
+    const glfw_extensions = glfw.getRequiredInstanceExtensions(&glfw_extension_count) orelse (&[_][*:0]const u8{}).ptr;
+
+    var extensions: std.ArrayList([*:0]const u8) = try .initCapacity(allocator, @intCast(glfw_extension_count + 1));
+    extensions.appendSliceAssumeCapacity(glfw_extensions[0..glfw_extension_count]);
+
+    if (enable_val_layers) {
+        extensions.appendAssumeCapacity(vk.extensions.ext_debug_utils.name.ptr);
+    }
+
+    return try extensions.toOwnedSlice();
 }
 
 // Define the function ourselves because glfw.getInstanceProcAddr doesn't use correct types
